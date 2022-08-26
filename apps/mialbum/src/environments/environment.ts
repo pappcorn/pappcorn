f97088204198e161481166ce1adfc0a00923603c
Nicolás Contreras Villegas <nicolas@vaki.co>
2022-08-26 05:43:32 -0300

    fix(core): remove firebase credentials for mialbum app.

--- apps/mialbum/src/environments/environment.ts
+++ apps/mialbum/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'mialbum-dev',
-    appId: '1:124812028575:web:c740fd09624331518050aa',
-    storageBucket: 'mialbum-dev.appspot.com',
-    locationId: 'us-east1',
-    apiKey: 'AIzaSyDeBRvgArX_mrpOs-fbqLzs8KITrgVrD4o',
-    authDomain: 'mialbum-dev.firebaseapp.com',
-    messagingSenderId: '124812028575',
-    measurementId: 'G-S668CXPH69',
-  },
+  firebase: {},
   production: false,
   hostUrl: 'http://localhost:4200',
 };
